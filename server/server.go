af96cbe7cbb10ff309a81b836a78bc141363c3a3
Vasu Pal <vasu@hypd.in>
2021-04-05 15:46:21 +0530

    enabled producer

--- server/server.go
+++ server/server.go
@@ -80,9 +80,10 @@ func NewServer() *Server {
 	// Initializing app and services
 	server.API.App = app.NewApp(&app.Options{MongoDB: ms, Logger: server.Log, Config: &c.APPConfig})
 	// server.API.App.Example = app.InitExample(&app.ExampleOpts{DBName: "example", MongoStorage: ms, Logger: server.Log})
+	app.InitProcessor(server.API.App)
 	app.InitService(server.API.App)
 	app.InitConsumer(server.API.App)
-	app.InitProcessor(server.API.App)
+	app.InitProducer(server.API.App)
 	return server
 }
 
