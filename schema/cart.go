23e07ff2c0984ffe3a4dd68b64d53229bdcee68b
Nilesh Matai <matainilesh678@gmail.com>
2021-04-06 16:06:46 +0530

    source id omitempty

--- schema/cart.go
+++ schema/cart.go
@@ -61,7 +61,7 @@ type OrderOpts struct {
 	ShippingAddress *OrderAddressOpts  `json:"shipping_address"`
 	BillingAddress  *OrderAddressOpts  `json:"billing_address"`
 	Source          string             `json:"source"`
-	SourceID        primitive.ObjectID `json:"source_id"`
+	SourceID        primitive.ObjectID `json:"source_id,omitempty"`
 	OrderItems      []OrderItem        `json:"order_items" bson:"order_items"`
 }
 
