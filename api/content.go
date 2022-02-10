5708b3255723903400b43798f74f9802274ef971
Nilesh <nileshmatai@Nileshs-MacBook-Pro.local>
2022-02-10 18:19:26 +0530

    log

--- api/content.go
+++ api/content.go
@@ -540,7 +540,8 @@ func (a *API) getPebblesForCreator(requestCTX *handler.RequestContext, w http.Re
 }
 
 func (a *API) contentProcessFail(requestCTX *handler.RequestContext, w http.ResponseWriter, r *http.Request) {
-	var s interface{}
+	var s map[string]interface{}
+
 	if err := a.DecodeJSONBody(r, &s); err != nil {
 		requestCTX.SetErr(err, http.StatusBadRequest)
 		return
