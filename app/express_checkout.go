4e4414fc7b28f61cb21bdb3273c7836c82ea04be
Nilesh <nileshmatai@Nileshs-MacBook-Pro.local>
2022-03-09 13:32:55 +0530

    added log

--- app/express_checkout.go
+++ app/express_checkout.go
@@ -536,6 +536,7 @@ func (ec *ExpressCheckoutImpl) ExpressCheckoutWeb(opts *schema.ExpressCheckoutWe
 				switch coupon.ApplicableON.Name {
 				case "brand":
 					if orderItem.CatalogInfo.BrandID == coupon.ApplicableON.IDs[0] {
+						fmt.Println("coupon brand match")
 						toAdd = true
 					}
 				case "influencer":
@@ -623,7 +624,7 @@ func (ec *ExpressCheckoutImpl) ExpressCheckoutWeb(opts *schema.ExpressCheckoutWe
 			AppliedValue: appliedValue,
 			ApplicableON: coupon.ApplicableON,
 		}
-		fmt.Println("coupon order opts ", couponOrderOpts)
+		fmt.Println("coupon order opts ", couponOrderOpts.ApplicableON.Name)
 	}
 
 	for brand, oi := range oiBrandMap {
