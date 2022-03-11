45dcf6fd111c2bdbd4a2c3ae47304726a5ef6f89
Nilesh <nileshmatai@Nileshs-MacBook-Pro.local>
2022-03-11 16:16:32 +0530

    fix source nil

--- app/express_checkout.go
+++ app/express_checkout.go
@@ -539,7 +539,7 @@ func (ec *ExpressCheckoutImpl) ExpressCheckoutWeb(opts *schema.ExpressCheckoutWe
 						toAdd = true
 					}
 				case "influencer":
-					if orderItem.Source.ID == coupon.ApplicableON.IDs[0].Hex() {
+					if orderItem.Source != nil && orderItem.Source.ID == coupon.ApplicableON.IDs[0].Hex() {
 						toAdd = true
 					}
 				case "cart":
@@ -562,7 +562,7 @@ func (ec *ExpressCheckoutImpl) ExpressCheckoutWeb(opts *schema.ExpressCheckoutWe
 						toAdd = true
 					}
 				case "influencer":
-					if orderItem.Source.ID == coupon.ApplicableON.IDs[0].Hex() {
+					if orderItem.Source != nil && orderItem.Source.ID == coupon.ApplicableON.IDs[0].Hex() {
 						toAdd = true
 					}
 				case "cart":
