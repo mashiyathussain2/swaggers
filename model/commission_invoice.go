4f769ad11c49de2c0a62e1bf8b4e37a2e91d1361
Nilesh <nileshmatai@Nileshs-MacBook-Pro.local>
2022-04-30 21:07:47 +0530

    added name  to template

--- model/commission_invoice.go
+++ model/commission_invoice.go
@@ -12,29 +12,43 @@ const (
 )
 
 type CommissionInvoice struct {
-	ID                primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	DebitRequestID    primitive.ObjectID `json:"debit_request_id,omitempty" bson:"debit_request_id,omitempty"`
-	InvoiceNo         string             `json:"invoice_no,omitempty" bson:"invoice_no,omitempty"`
-	InfluencerID      primitive.ObjectID `json:"influencer_id,omitempty" bson:"influencer_id,omitempty"`
-	InfluencerInfo    Influencer         `json:"influencer_info,omitempty" bson:"influencer_info,omitempty"`
-	UserInfo          User               `json:"user_info,omitempty" bson:"user_info,omitempty"`
-	Amount            uint               `json:"amount,omitempty" bson:"amount,omitempty"`
-	PayoutInformation *PayoutInformation `json:"payout_information,omitempty" bson:"payout_information,omitempty"`
-	RequestDate       time.Time          `json:"request_date,omitempty" bson:"request_date,omitempty"`
-	CreatedAt         time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	ID                primitive.ObjectID       `json:"id,omitempty" bson:"_id,omitempty"`
+	DebitRequestID    primitive.ObjectID       `json:"debit_request_id,omitempty" bson:"debit_request_id,omitempty"`
+	InvoiceNo         string                   `json:"invoice_no,omitempty" bson:"invoice_no,omitempty"`
+	InfluencerID      primitive.ObjectID       `json:"influencer_id,omitempty" bson:"influencer_id,omitempty"`
+	InfluencerInfo    InfluencerInfoForInvoice `json:"influencer_info,omitempty" bson:"influencer_info,omitempty"`
+	UserInfo          UserInfoForInvoice       `json:"user_info,omitempty" bson:"user_info,omitempty"`
+	Amount            uint                     `json:"amount,omitempty" bson:"amount,omitempty"`
+	PayoutInformation *PayoutInformation       `json:"payout_information,omitempty" bson:"payout_information,omitempty"`
+	RequestDate       time.Time                `json:"request_date,omitempty" bson:"request_date,omitempty"`
+	CreatedAt         time.Time                `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
 
 type DebitRequestAllInfo struct {
+	ID                primitive.ObjectID         `json:"id,omitempty" bson:"_id,omitempty"`
+	InfluencerID      primitive.ObjectID         `json:"influencer_id,omitempty" bson:"influencer_id,omitempty"`
+	Amount            float64                    `json:"amount,omitempty" bson:"amount,omitempty"`
+	Status            string                     `json:"status,omitempty" bson:"status,omitempty"`
+	PayoutInformation *PayoutInformation         `json:"payout_information,omitempty" bson:"payout_information,omitempty"`
+	InfluencerInfo    []InfluencerInfoForInvoice `json:"influencer_info,omitempty" bson:"influencer_info,omitempty"`
+	UserInfo          []UserInfoForInvoice       `json:"user_info,omitempty" bson:"user_info,omitempty"`
+	GranteeID         primitive.ObjectID         `json:"grantee_id,omitempty" bson:"grantee_id,omitempty"`
+	CreatedAt         time.Time                  `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt         time.Time                  `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+}
+
+type UserInfoForInvoice struct {
+	ID      primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Email   string             `json:"email,omitempty" bson:"email,omitempty"`
+	PhoneNo *PhoneNumber       `json:"phone_no,omitempty" bson:"phone_no,omitempty"`
+}
+
+type InfluencerInfoForInvoice struct {
 	ID                primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	InfluencerID      primitive.ObjectID `json:"influencer_id,omitempty" bson:"influencer_id,omitempty"`
-	Amount            float64            `json:"amount,omitempty" bson:"amount,omitempty"`
-	Status            string             `json:"status,omitempty" bson:"status,omitempty"`
+	Name              string             `json:"name,omitempty" bson:"name,omitempty"`
 	PayoutInformation *PayoutInformation `json:"payout_information,omitempty" bson:"payout_information,omitempty"`
-	InfluencerInfo    []Influencer       `json:"influencer_info,omitempty" bson:"influencer_info,omitempty"`
-	UserInfo          []User             `json:"user_info,omitempty" bson:"user_info,omitempty"`
-	GranteeID         primitive.ObjectID `json:"grantee_id,omitempty" bson:"grantee_id,omitempty"`
-	CreatedAt         time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt         time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	Balance           uint               `json:"balance,omitempty" bson:"balance,omitempty"`
+	TotalCommission   uint               `json:"total_commission,omitempty" bson:"total_commission,omitempty"`
 }
 
 // InvoiceDate returns invoice creation date in IST timezone
