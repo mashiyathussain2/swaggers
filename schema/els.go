0a6e718d00d6d773b87ff3f5415f51b8aafc35cc
Nilesh <nileshmatai@Nileshs-MacBook-Pro.local>
2022-05-02 12:42:06 +0530

    sending null for policy if not added

--- schema/els.go
+++ schema/els.go
@@ -40,7 +40,7 @@ type GetBrandInfoEsResp struct {
 	CreatedAt        time.Time            `json:"created_at,omitempty"`
 	UpdatedAt        time.Time            `json:"updated_at,omitempty"`
 	IsFollowedByUser bool                 `json:"is_followed_by_user,omitempty"`
-	Policies         []model.Policy       `json:"policies,omitempty"`
+	Policies         []model.Policy       `json:"policies"`
 	IsCODAvailable   bool                 `json:"is_cod_available"`
 }
 
