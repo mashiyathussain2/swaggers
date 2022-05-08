f41bc76f7f9123d9866ffe99ee3025213e990a0b
Mashiyat Hussain <mashiyat.hussain@hypd.in>
2022-05-08 14:26:52 +0530

    Added extension x tag

--- api/docs.go
+++ api/docs.go
@@ -3,6 +3,22 @@
 //  Schemes: https
 //  Host: cms.getshitdone.in
 //  BasePath: /api
+//
+//
+//
+//   Extensions:
+//     x-tagGroups:
+//       - name: Stardust
+//         description: This is the stardust api.
+//         tags:
+//           - AppCategory
+//           - LiveStream
+//           - Pebble
+//           - content
+//           - UploadImage
+//           - live
+//         x-traitTag: true
+//
 //  Produces:
 //    - application/json
 //
